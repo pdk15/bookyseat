6f9dcf341b2b3b3cfc01d980f6eb30a8f38bce8b
Pranav Khanolkar <pranavkhanolkar589@gmail.com>
2026-05-26 11:07:46 +0530

    first commit

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&qryc0+bp3(jllp=&1vx+otqi-pm0gh%s%=b@i8grhg!uis0na
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
